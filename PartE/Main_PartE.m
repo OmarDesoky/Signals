26dcfc5ae48979e5de556607977732f1ea4eb1ed
Omar <IamAdeadfish@hotmail.com>
2019-05-10 22:57:39 +0200

    modified partE

--- PartE/Main_PartE.m
+++ PartE/Main_PartE.m
@@ -1,33 +1,27 @@
                                         #Main
                                         #Part E
                                         
-# Part E-a
-
-arr = []
-arr2= []
-for i =-10:1:10
-  t=i;
-  if i==0;
-    t=0.0000001;
-  endif
-  m= sin(5*pi*t)/(pi*t);
-  r = m* cos(30*pi*t );
-  arr2= [arr2 r];    
-  arr = [arr m];
-endfor
-
-#plotting  m(t)
-subplot(2,1,1);
-y =fft(arr);
-range = -10:1:10;
-mag=abs(y);
-plot (range,mag);                  
-########################################################
-
-#Part B-b
-#plotting  r(t)
-subplot(2,1,2);
-z =fft(arr2);
-mag2=abs(z);
-plot (range,mag2);     
-     
+t=[-100:0.01:100];
+#plotting m(t) function from -10 to 10
+subplot(4,1,1);
+m =5.*sinc(5*t); 
+plot(t,m) ;
+axis([-10 10]);
+#plotting m(t) function after doing fourier transform from -50 to 50
+subplot(4,1,2);
+fourier_transform_m = fft (m);
+FTM_shifted_m = fftshift(fourier_transform_m);
+plot (t,abs(FTM_shifted_m));
+axis([-50 50]);
+#plotting r(t) function from -10 to 10
+subplot(4,1,3);
+r = m.* cos(30*pi*t );
+plot(t,r) ;
+axis([-10 10]);
+#plotting r(t) after doing  doing fourier transform from -50 to 50
+subplot(4,1,4);
+fourier_transform_r= fft (r);
+FTM_shifted_r = fftshift(abs(fourier_transform_r));
+t=[-57:0.0057:57];
+plot (t, abs(FTM_shifted_r));
+axis([-50 50]);
