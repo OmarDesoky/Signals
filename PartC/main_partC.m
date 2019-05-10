fbe7b2f54292241a5bf8578b6eb70a2434155546
Omar <IamAdeadfish@hotmail.com>
2019-05-10 22:57:07 +0200

    modified energy/power Results

--- PartC/main_partC.m
+++ PartC/main_partC.m
@@ -19,10 +19,10 @@ endfor
 
 [z_square] =@(t) z(t).^2;
 
-energy =integral(z_square,0,5) *5 ; # equivalent to : energy =integral (z,0,5);
+energy =integral(z_square,0,5) ; # equivalent to : energy =integral (z,0,5);
 interval = 15 - (-15);
 power = energy / interval;
 
 msgbox(
-strcat("energy = " ,mat2str(energy),"\n","power = ",mat2str(power))
+strcat("energy = " ,mat2str(Inf),"\n","power = ",mat2str(power))
 );
